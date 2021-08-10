08e7aba5d648aedb8071235f3edc800c83eea925
David <djknit@gmail.com>
2021-08-10 15:17:31 -0500

    initial java project created

---
+++ src/com/cognixia/jump/DollarsBankDriver.java
@@ -0,0 +1,9 @@
+package com.cognixia.jump;
+
+public class DollarsBankDriver {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/com/cognixia/jump/model/Patron.java
@@ -0,0 +1,50 @@
+package com.cognixia.jump.model;
+
+public class Patron {
+
+	private String username;
+	private String password;
+	private Long userId;
+	private String name;
+	private String address;
+	private String phoneNumber;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+}
