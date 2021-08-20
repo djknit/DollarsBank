eaf0b4d64bf658ce3bee8efe4ca1d452bc0bfaa4
David <djknit@gmail.com>
2021-08-20 17:06:52 -0500

    got colors working in my eclipse console. created colors utility.

--- src/com/cognixia/jump/DollarsBankDriver.java
+++ src/com/cognixia/jump/DollarsBankDriver.java
@@ -5,42 +5,9 @@ import com.cognixia.jump.utility.Display;
 import com.cognixia.jump.utility.FileIO;
 
 public class DollarsBankDriver {
-
-    public static final String RESET = "\033[0m";  
-	public static final String TEXT_BLACK = "\u001B[30m";
-	public static final String TEXT_RED = "\u001B[31m";
-	public static final String TEXT_GREEN = "\u001B[32m";
-	public static final String TEXT_YELLOW = "\u001B[33m";
-	public static final String TEXT_BLUE = "\u001B[34m";
-	public static final String TEXT_PURPLE = "\u001B[35m";
-	public static final String TEXT_CYAN = "\u001B[36m";
-	public static final String TEXT_WHITE = "\u001B[37m";
-	
-
-    // Regular Colors
-    public static final String BLACK = "\033[0;30m";   // BLACK
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String GREEN = "\033[0;32m";   // GREEN
-    public static final String YELLOW = "\033[0;33m";  // YELLOW
-    public static final String BLUE = "\033[0;34m";    // BLUE
-    public static final String PURPLE = "\033[0;35m";  // PURPLE
-    public static final String CYAN = "\033[0;36m";    // CYAN
-    public static final String WHITE = "\033[0;37m"; 
     
-    public static final char ESC = (char) 27;
-	
 	public static void main(String[] args) {
-//		System.out.println("test");
-//		System.out.println(ESC + "[32m" + "green test" + ESC + "[0m");
-//
-//		System.out.println("\u001B[36m" + "cyan test" + "\u001B[0m");
-//
-//		System.out.println("\naayee\n");
-//		Display.printDivider();
-//		System.out.println("\n\naayee");
-//		
-//		FileIO dummy = new FileIO();
-		
+
 		Display.printLogo();
 		new LandingMenu().run();
 	}
