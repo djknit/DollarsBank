c5551b9f0fc003f14c7982949581d75a4bae9cf4
David <djknit@gmail.com>
2021-08-20 23:13:07 -0500

    turned Colors utility into enum and completed methods

--- src/com/cognixia/jump/utility/Colors.java
+++ src/com/cognixia/jump/utility/Colors.java
@@ -1,19 +1,37 @@
 package com.cognixia.jump.utility;
 
-public class Colors {
-	
-	/* Note: An idea for password concealing found @:
-	 * 		http://www.cse.chalmers.se/edu/year/2018/course/TDA602/Eraserlab/pwdmasking.html
-	 *  */
+public enum Colors {
 
-    public static final String TEXT_RESET = "\u001b[0m";  
-	public static final String TEXT_BLACK = "\u001b[30m";
-	public static final String TEXT_RED = "\u001b[31m";
-	public static final String TEXT_GREEN = "\u001b[32m";
-	public static final String TEXT_YELLOW = "\u001b[33m";
-	public static final String TEXT_BLUE = "\u001B[34m";
-	public static final String TEXT_MAGENTA = "\u001B[35m";
-	public static final String TEXT_CYAN = "\u001B[36m";
-	public static final String TEXT_WHITE = "\u001B[37m";
+	RESET("\u001b[0m"),
+	BLACK("\u001b[30m"),
+	RED("\u001b[31m"),
+	GREEN("\u001b[32m"),
+	YELLOW("\u001b[33m"),
+	BLUE("\u001b[34m"),
+	MAGENTA("\u001b[35m"),
+	CYAN("\u001b[36m"),
+	WHITE("\u001b[37m");
+
+	private final String ansiCode;
+	
+	Colors(String ansiCode) {
+		this.ansiCode = ansiCode;
+	}
+	
+	public String colorize(String noncoloredText) {
+		return ansiCode + noncoloredText + RESET;
+	}
+	
+	public void startConsoleColor() {
+		System.out.print(ansiCode);
+	}
+	
+	public static void resetConsoleColor() {
+		System.out.print(RESET);
+	}
 	
 }
+
+/* NOTE TO SELF: An idea for password concealing found @:
+ * 		http://www.cse.chalmers.se/edu/year/2018/course/TDA602/Eraserlab/pwdmasking.html
+ *  */
